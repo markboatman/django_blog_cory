5944f35c2404b85bcdc4c41b1fc8a54aa8c89391
Mark Boatman <markjboat@yahoo.com>
2023-01-03 16:23:53 +0800

    Added STATIC_ROOT defined to BASE_DIR/staticfiles to settings.py

--- corys_blog/settings.py
+++ corys_blog/settings.py
@@ -153,6 +153,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
+# add this for heroku, heroku will put static files here
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
 
